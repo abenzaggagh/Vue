7034eb340705f569ce6e3d8ac53eba42c95fda27
Amine BEN ZAGGAGH <amine.benzaggagh@icloud.com>
2025-03-28 12:05:02 +0100

    Robot Builder

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" /> Build-a-Bot
+        </li>
+      </ul>
+    </nav>
+  </header>
   <main>
-    <HomePage msg="Welcome to Your Vue.js App"/>
+    <!-- <HomePage /> -->
+     <RobotBuilder />
   </main>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HomePage,
+    // HomePage,
+    RobotBuilder,
   },
 };
 </script>
@@ -31,5 +43,28 @@ main {
   background-color: white;
   width: 1400px;
   min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1460px;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -18,20 +18,17 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.home {
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.robot {
+  height: 500px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.get-started {
+  padding-top: 20px;
+  font-size: 32px;
 }
 </style>
